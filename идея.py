4a1b1496f03c349fc6a5d99ca4529ad1cac0d114
Tisaka <97775364+Tisaka@users.noreply.github.com>
2022-08-27 12:43:14 +0700

    Add files via upload

---
+++ идея.py
@@ -0,0 +1,52 @@
+import pygame
+import sys
+
+pygame.init()
+size_block = 100
+margin = 15
+width = heigth = size_block * 3 + margin * 4
+
+size_window = (width, heigth)
+screen = pygame.display.set_mode(size_window)
+pygame.display.set_caption("Крестики-нолики")
+
+black = (0, 0, 0)
+pink = (255, 0, 255)
+sky_blue = (0, 255, 255)
+white = (255, 255, 255)
+mas = [[0]*3 for i in range(3)]
+query = 0
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit(0)
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            x_mouse, y_mouse = pygame.mouse.get_pos()
+            col = x_mouse // (size_block + margin)
+            row = y_mouse // (size_block + margin)
+            if mas[row][col] == 0:
+                if query%2 == 0:
+                    mas[row][col] = 'x'
+                else:
+                    mas[row][col] = 'o'
+                query += 1
+
+    for row in range(3):
+        for col in range(3):
+            if mas[row][col] == 'x':
+                color = pink
+            elif mas[row][col] == 'o':
+                color = sky_blue
+            else:
+                color = white
+            x = col * size_block + (col + 1) * margin
+            y = row * size_block + (row + 1) * margin
+            pygame.draw.rect(screen, color, (x, y, size_block, size_block))
+            if color == pink:
+                pygame.draw.line(screen, white, (x + 5, y + 5), (x + size_block - 5, y + size_block - 5), 3)
+                pygame.draw.line(screen, white, (x + size_block - 5, y + 5), (x + 5, y + size_block - 5), 3)
+            elif color == sky_blue:
+                pygame.draw.circle(screen, white, (x + size_block // 2, y + size_block // 2), size_block // 2 - 3, 3)
+    pygame.display.update()
